a584cdacb0866c07c68ba3c2953160d5b2294d56
alessa-lou <alessa_hardwick@live.co.uk>
2020-11-12 11:56:11 +0000

    deleted some stuff that didnt work

---
+++ interface.js
@@ -0,0 +1,45 @@
+
+$(document).ready(function() {
+    var thermostat = new Thermostat();
+    updateTemperature();
+  
+    $('#temperature-up').click(function() {
+    thermostat.up();
+    updateTemperature();
+    });
+  
+    $('#temperature-down').click(function() {
+      thermostat.down();
+      updateTemperature();
+      });
+  
+    $('#temperature-reset').click(function() {
+      thermostat.resetTemperature();
+      updateTemperature();
+    });
+  
+    $('#powersaving-off').click(function() {
+      thermostat.switchPowerSavingModeOff();
+      $('#power-saving-status').text('off');
+    });
+  
+    $('#powersaving-on').click(function () {
+      thermostat.switchPowerSavingModeOn();
+      $('#power-saving-status').text('on');
+      updateTemperature();
+    });
+  
+  
+  
+  
+  
+    function updateTemperature() {
+      $('#temperature').text(`Current Temperature: ${thermostat.getCurrentTemperature()} degrees`);
+      $('#temperature').attr('class', thermostat.energyUsage());
+    }
+  
+  
+  
+  
+  
+  })
