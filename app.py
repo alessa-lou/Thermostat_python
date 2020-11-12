452b639c1e0df66fbebdfbcbc8d858c0a01f7de6
alessa-lou <alessa_hardwick@live.co.uk>
2020-11-12 13:14:00 +0000

    added post route and some JS to interface.js

--- app.py
+++ app.py
@@ -1,10 +1,40 @@
 from flask import Flask, render_template, url_for, request, redirect
+from thermostat import Thermostat
+
+import json
+from flask import jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/temperature')
+def temp():
+    thermostat = Thermostat()
+    list = {
+        "temperature": thermostat.temperature,
+        "power_saving_mode": thermostat.is_PSM_on(),
+        "energy_usage": thermostat.energy_usage(),
+        "status": 200
+
+    }    
+    return jsonify(results = list)
+
+@app.route('/temperature', methods=['POST'])
+def post():
+    thermostat = Thermostat()
+    if "up":
+        return thermostat.up()
+    elif "down":
+        return thermostat.down()
+    elif "reset":
+        return thermostat.reset_temp()
+    else:
+        return "?"
+    jsonify(status=200)
+    
 
 if __name__ == '__main__':
     app.run()
--- interface.js
+++ interface.js
@@ -1,47 +1,61 @@
 // ??? something useful needs to go in here...
-
+'use strict';
 
 $(document).ready(function() {
-    var thermostat = new Thermostat();
     updateTemperature();
   
     $('#temperature-up').click(function() {
-    thermostat.up();
-    updateTemperature();
+        $.post('/temperature', { method: "up"}, function(res){
+            var data = JSON.parse(res);
+            if (data.status == 200) {
+                updateTemperature();
+            }
+        });
     });
   
-    $('#temperature-down').click(function() {
-      thermostat.down();
-      updateTemperature();
-      });
-  
-    $('#temperature-reset').click(function() {
-      thermostat.resetTemperature();
-      updateTemperature();
+    $('#temperature-up').click(function() {
+        $.post('/temperature', { method: "down"}, function(res){
+            var data = JSON.parse(res);
+            if (data.status == 200) {
+                updateTemperature();
+            }
+        });
     });
   
-    $('#powersaving-off').click(function() {
-      thermostat.switchPowerSavingModeOff();
-      $('#power-saving-status').text('off');
+    $('#temperature-up').click(function() {
+        $.post('/temperature', { method: "reset"}, function(res){
+            var data = JSON.parse(res);
+            if (data.status == 200) {
+                updateTemperature();
+            }
+        });
     });
   
-    $('#powersaving-on').click(function () {
-      thermostat.switchPowerSavingModeOn();
-      $('#power-saving-status').text('on');
-      updateTemperature();
-    });
+    // $('#powersaving-off').click(function() {
+    //   thermostat.switchPowerSavingModeOff();
+    //   $('#power-saving-status').text('off');
+    // });
+  
+    // $('#powersaving-on').click(function () {
+    //   thermostat.switchPowerSavingModeOn();
+    //   $('#power-saving-status').text('on');
+    //   updateTemperature();
+    // });
   
   
   
   
   
     function updateTemperature() {
-      $('#temperature').text(`Current Temperature: ${thermostat.getCurrentTemperature()} degrees`);
-      $('#temperature').attr('class', thermostat.energyUsage());
-    }
+        $.get('/temperature', function(res) {
+            var data = JSON.parse(res)
+            if (data.status == 200) {
+                var power_saving_mode = data.is_PSM_on ? "True" : "False";
+                $('#temperature').text(`Current Temperature: ${thermostat.getCurrentTemperature()} degrees`);
+                $('#temperature').attr('class', thermostat.energyUsage());
+                $('#power-saving-status').text(power_saving_mode);
+            }
+        });
+    }  
   
-  
-  
-  
-  
-  })
+  });
